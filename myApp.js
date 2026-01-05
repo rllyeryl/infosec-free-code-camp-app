8ae9d49a63d907b6de0e417503b12ebb0add156d
rllyeryl <sendtoejana@gmail.com>
2026-01-05 17:57:27 +0800

    Added use of helmet.hidePoweredby

--- myApp.js
+++ myApp.js
@@ -4,13 +4,15 @@ const app = express();
 
 const api = require('./server.js');
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet());
+
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 
 app.use('/_api', api);
 
-app.use(helmet.hidePoweredBy());
-app.disable('x-powered-by');
+
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
@@ -23,20 +25,6 @@ app.listen(port, () => {
 });
 
 
-/*app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
-
-module.exports = app;
-
-
-
-
-
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});*/
 
 
 
