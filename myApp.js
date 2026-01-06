129c34c8d1ffd06ce4232875231961f2f3dbe2ab
rllyeryl <sendtoejana@gmail.com>
2026-01-06 13:09:54 +0800

    added helmet

--- myApp.js
+++ myApp.js
@@ -4,69 +4,19 @@ const app = express();
 
 const api = require('./server.js');
 
-app.use(helmet());
+// FCC expects ONLY this
 app.use(helmet.hidePoweredBy());
-app.disable('x-powered-by');
 
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 
 app.use('/_api', api);
 
-
-
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-  console.log(`Eryl Jana Carillo Information Security App started on Port ${port}`);
+  console.log(`Your app is listening on port ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
